2a51c79968c0dc3f422b6bdd3f6622c5023b1b4c
NuclearOhs <krileyohalloran@hotmail.com>
2025-12-08 22:21:45 -0800

    Mock UI now matches the Presentation Mock UI, all dummy buttons

--- src/BioreactorUI/MainWindow.xaml.cs
+++ src/BioreactorUI/MainWindow.xaml.cs
@@ -12,15 +12,6 @@ namespace BioreactorUI
             var reactor = new ReactorModel(1, 1);
             var motor = new MotorModel(0);
 
-            OutputText.Text = $"Hello, World!\n" +
-                              $"Starting Project\n" +
-                              $"Starting Execution Manager\n" +
-                              $"Connecting To Reactor\n" +
-                              $"Reactor ID: {reactor.ReactorID}\n" +
-                              $"Number of Motors: {reactor.NumberOfMotors}\n" +
-                              $"Reactor Connection Successful: {reactor.ConnectionSuccessful}\n" +
-                              $"Executing Project of ID: 0\n" +
-                              $"{motor.StatusMessage}";
         }
     }
 }
