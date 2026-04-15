0b60d30c8d3a2c9f77f9db5fe6b005709ce4e970
Pandawaflez <carsongusta@gmail.com>
2026-04-15 16:50:19 -0700

    tweak: pushing remaining integration work

--- src/BioreactorControl/obj/Debug/net10.0/BioreactorControl.AssemblyInfo.cs
+++ src/BioreactorControl/obj/Debug/net10.0/BioreactorControl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BioreactorControl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84738226588dab175b39550a95d7a5b9b5fe6346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675374818d8d25439151edf1e265518350743ff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BioreactorControl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BioreactorControl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
