7426452d9d34a21bde10c83b1c1d760523a28240
Emily Smith <emil3678@vandals.uidaho.edu>
2026-04-21 17:26:04 -0700

    pause print

--- src/BioreactorControl/PythonClient.cs
+++ src/BioreactorControl/PythonClient.cs
@@ -10,6 +10,7 @@ public class PythonMotorClient
     public async Task MoveAbsolute(string motor, float target)
     {
         Console.WriteLine("move absolute in python client: ", _baseUrl, "/motor/move-absolute");
+        sleep(1);
         await _http.PostAsJsonAsync($"{_baseUrl}/motor/move-absolute",
             new { motor, target });
     }
